a8ed2f1abd98e7063b0d0855a3b9db6643bec48a
Victor <victor.j.zhao@gmail.com>
2022-03-08 22:28:41 -0500

    added for each EquipmentBundle constraint

--- src/main/java/ca/mcgill/ecse/divesafe/controller/BundleController.java
+++ src/main/java/ca/mcgill/ecse/divesafe/controller/BundleController.java
@@ -1,11 +1,28 @@
 package ca.mcgill.ecse.divesafe.controller;
 
 import java.util.List;
+
+import ca.mcgill.ecse.divesafe.application.DiveSafeApplication;
 import ca.mcgill.ecse.divesafe.model.EquipmentBundle;
 
 public class BundleController {
   public static String addEquipmentBundle(String name, int discount, List<String> equipmentNames,
       List<Integer> equipmentQuantities) {
+        var error = "";
+      
+        if(discount < 0) {
+          error = "The discount must be greater than zero. "; 
+        }
+
+        if(discount > 100) {
+          error = "The discount must be less than one hundred. ";
+        }
+
+        if(!error.isEmpty()) {
+          return error.trim();
+        }
+
+
     return null;
   }
 
