c85765b51eb257d8ea26d15da4c7ca8e046faff1
Victor <victor.j.zhao@gmail.com>
2022-03-08 20:01:28 -0500

    Added @Then tests

--- src/test/java/ca/mcgill/ecse/divesafe/features/AddEquipmentBundleStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/divesafe/features/AddEquipmentBundleStepDefinitions.java
@@ -20,7 +20,7 @@ public class AddEquipmentBundleStepDefinitions {
 
   // Instance Variables
   private DiveSafe divesafe;
-
+  private String error;
   /**
    * @author Siger Ma
    */
@@ -34,6 +34,7 @@ public class AddEquipmentBundleStepDefinitions {
     int aPriceOfGuidePerDay = Integer.parseInt(rows.get(0).get("priceOfGuidePerDay"));
 
     divesafe = new DiveSafe(aStartDate, aNumDays, aPriceOfGuidePerDay);
+    error = ""; //Ask question !!!
 
   }
 
@@ -58,10 +59,6 @@ public class AddEquipmentBundleStepDefinitions {
   }
   /**
    * @author Eric Joung
-   * @param string
-   * @param string2
-   * @param string3
-   * @param string4
    */
   @When("the administrator attempts to add an equipment bundle with name {string}, discount {string}, items {string}, and quantities {string} \\(p2)")
   public void the_administrator_attempts_to_add_an_equipment_bundle_with_name_discount_items_and_quantities_p2(
@@ -78,28 +75,30 @@ public class AddEquipmentBundleStepDefinitions {
     BundleController.addEquipmentBundle(string, Integer.parseInt(string2), itemList, quantitiesList);
   }
 
+  /**
+   * @author Kevin Luo
+   */
+
   @Then("the number of equipment bundles in the system shall be {string} \\(p2)")
   public void the_number_of_equipment_bundles_in_the_system_shall_be_p2(String numberOfBundles) {
-    // Write code here that turns the phrase above into concrete actions
-
-    /**
-     * @author Kevin Luo
-     */
     assertEquals(Integer.parseInt(numberOfBundles), divesafe.getBundles().size());
 
   }
 
+  /**
+   * @author Jiahao Zhao
+   */
+
   @Then("the equipment bundle {string} shall exist in the system \\(p2)")
-  public void the_equipment_bundle_shall_exist_in_the_system_p2(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  public void the_equipment_bundle_shall_exist_in_the_system_p2(String bundleName) {
+    assertEquals(bundleName, divesafe.getBundle(0));
   }
 
+  /**
+   * @author Kevin Luo
+   */
   @Then("the equipment bundle {string} shall contain the items {string} with quantities {string} \\(p2)")
   public void the_equipment_bundle_shall_contain_the_items_with_quantities_p2(String bundleName,
-      /**
-       * @author Kevin Luo
-       */
       String bundleItems, String equipmentQuantities) {
     // Write code here that turns the phrase above into concrete actions
 
@@ -117,26 +116,31 @@ public class AddEquipmentBundleStepDefinitions {
     }
 
     assertEquals(equipmentQuantities, quantityItem);
-
   }
+
+  /**
+   * @author Jiahao Zhao
+   */
 
   @Then("the equipment bundle {string} shall have a discount of {string} \\(p2)")
-  public void the_equipment_bundle_shall_have_a_discount_of_p2(String string, String string2) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  public void the_equipment_bundle_shall_have_a_discount_of_p2(String bundleName, String bundleDiscount) {
+    assertEquals(bundleName, divesafe.getBundle(0));
+    assertEquals(bundleDiscount, divesafe.getBundle(0).getDiscount());
   }
 
+  /**
+   * @author Jiahao Zhao
+   */
+
   @Then("the error {string} shall be raised \\(p2)")
-  public void the_error_shall_be_raised_p2(String string) {
-    // Write code here that turns the phrase above into concrete actions
-
-
-    throw new io.cucumber.java.PendingException();
+  public void the_error_shall_be_raised_p2(String errorMsg) {
+    assertTrue(error.contains(errorMsg));
   }
 
   /**
    * @author Siger Ma
    */
+
   @Given("the following equipment bundles exist in the system: \\(p2)")
   public void the_following_equipment_bundles_exist_in_the_system_p2 (io.cucumber.datatable.DataTable dataTable) {
 
@@ -174,5 +178,7 @@ public class AddEquipmentBundleStepDefinitions {
   public void tearDown() {
     divesafe.delete();
   }
+
+    //callController method??
 
 }
