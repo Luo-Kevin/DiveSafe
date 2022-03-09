359dbecb1526f716871d97d569ae344c7b2b7b90
Siger Ma <siger.ma@mail.mcgill.ca>
2022-03-08 22:31:04 -0500

    Bundle controller added create bundle and bundle items

--- src/main/java/ca/mcgill/ecse/divesafe/controller/BundleController.java
+++ src/main/java/ca/mcgill/ecse/divesafe/controller/BundleController.java
@@ -1,11 +1,32 @@
 package ca.mcgill.ecse.divesafe.controller;
 
 import java.util.List;
+
+import ca.mcgill.ecse.divesafe.application.DiveSafeApplication;
+import ca.mcgill.ecse.divesafe.model.DiveSafe;
+import ca.mcgill.ecse.divesafe.model.Equipment;
 import ca.mcgill.ecse.divesafe.model.EquipmentBundle;
+import ca.mcgill.ecse.divesafe.model.Item;
 
 public class BundleController {
-  public static String addEquipmentBundle(String name, int discount, List<String> equipmentNames,
-      List<Integer> equipmentQuantities) {
+
+  // Instance variables
+  private static DiveSafe divesafe = DiveSafeApplication.getDiveSafe();
+
+  public static String addEquipmentBundle(String name, int discount, List<String> equipmentNames, List<Integer> equipmentQuantities) {
+    
+    // Create bundle by SM
+    EquipmentBundle aBundle = divesafe.addBundle(name, discount);
+
+    // Add bundle items by SM
+    for (int i = 0; i < equipmentNames.size(); i++) {
+
+      Equipment aEquipment = (Equipment) Item.getWithName(equipmentNames.get(i));
+      int aQuantity = equipmentQuantities.get(i);
+
+      divesafe.addBundleItem(aQuantity, aBundle, aEquipment);
+    }
+
     return null;
   }
 
