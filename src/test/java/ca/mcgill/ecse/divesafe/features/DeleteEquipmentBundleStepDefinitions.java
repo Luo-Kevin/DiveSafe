e4a0042a2ca3f86772f19b02a95ee922b0af588d
Zahra Landou <zahra.landou@mail.mcgill.ca>
2022-03-12 19:10:00 -0500

    implemented some @Given clauses in DeleteEquipmentBundleStepDefinitions

--- src/test/java/ca/mcgill/ecse/divesafe/features/DeleteEquipmentBundleStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/divesafe/features/DeleteEquipmentBundleStepDefinitions.java
@@ -1,59 +1,96 @@
 package ca.mcgill.ecse.divesafe.features;
 
+
+import java.util.*;
+import java.sql.Date;
+import ca.mcgill.ecse.divesafe.application.DiveSafeApplication;
+import ca.mcgill.ecse.divesafe.model.DiveSafe;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class DeleteEquipmentBundleStepDefinitions {
+//Instance variables
+private DiveSafe divesafe;
+private String error;
+
+/**
+   * @author Zahra Landou
+   */
+
   @Given("the following DiveSafe system exists: \\(p6)")
   public void the_following_dive_safe_system_exists_p6(io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    List<Map<String, String>> rows = dataTable.asMaps();
+
+    Date aStartDate = (Date)Date.valueOf(rows.get(0).get("startDate"));
+    int aNumDays = Integer.parseInt(rows.get(0).get("numDays"));
+    int aPriceOfGuidePerDay = Integer.parseInt(rows.get(0).get("priceOfGuidePerDay"));
+
+    DiveSafeApplication.setDiveSafe(aStartDate, aNumDays, aPriceOfGuidePerDay);
+    divesafe = DiveSafeApplication.getDiveSafe();
+    error = "";
+   // throw new io.cucumber.java.PendingException();
   }
+
+/**
+ * 
+ * @author Zahra Landou
+ */
 
   @Given("the following equipment exists in the system: \\(p6)")
   public void the_following_equipment_exists_in_the_system_p6(
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    List<Map<String,String>> rows = dataTable.asMaps();
+    for (var row : rows) {
+
+      String aName = row.get("name");
+      int aWeight = Integer.parseInt(row.get("weight"));
+      int aPricePerDay = Integer.parseInt(row.get("pricePerDay"));
+
+      divesafe.addEquipment(aName, aWeight, aPricePerDay);
+
+    }
+    
+  //  throw new io.cucumber.java.PendingException();
   }
+   /**
+    * 
+    * @author Zahra Landou
+    */
 
   @Given("the following equipment bundles exist in the system: \\(p6)")
   public void the_following_equipment_bundles_exist_in_the_system_p6(
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    List<Map<String, String>> rows = dataTable.asMaps();
+
+  
+
+
+    //throw new io.cucumber.java.PendingException();
   }
+  /**
+   * 
+   * @author Zahra Landou
+   */
+
 
   @Given("the following members exist in the system: \\(p6)")
   public void the_following_members_exist_in_the_system_p6(
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    
+    List<Map<String, String>> rows = dataTable.asMaps();
+    String aEmail = rows.get(0).get("email");
+    String aPassword = rows.get(0).get("password");
+    String aName = rows.get(0).get("name");
+    String aEmergencyContact =  rows.get(0).get("emergencyContact");
+    int aNumDays = Integer.parseInt(rows.get(0).get("numDays"));
+    boolean aGuideRequired = Boolean.parseBoolean(rows.get(0).get("guideRequired"));
+    boolean aHotelRequired = Boolean.parseBoolean(rows.get(0).get("hotelRequired"));
+    
+    divesafe.addMember(aEmail, aPassword, aName, aEmergencyContact, aNumDays, aGuideRequired, aHotelRequired);
+    
+    //List <String> itemBookList = new ArrayList<String>(Arrays.asList(arg0))
+    
   }
 
   @When("the administrator attempts to delete the equipment bundle {string} \\(p6)")
@@ -71,33 +108,22 @@ public class DeleteEquipmentBundleStepDefinitions {
   @Then("the equipment bundle {string} shall not exist in the system \\(p6)")
   public void the_equipment_bundle_shall_not_exist_in_the_system_p6(String string) {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+   // throw new io.cucumber.java.PendingException();
   }
 
   @Then("the equipment bundle {string} shall preserve the following properties: \\(p6)")
   public void the_equipment_bundle_shall_preserve_the_following_properties_p6(String string,
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    
+    //throw new io.cucumber.java.PendingException();
   }
 
   @Then("the member {string} shall have the following bookable items: \\(p6)")
   public void the_member_shall_have_the_following_bookable_items_p6(String string,
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+   
+
+    //throw new io.cucumber.java.PendingException();
   }
 
   @Then("the number of pieces of equipment in the system shall be {string} \\(p6)")
