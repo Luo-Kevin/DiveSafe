847c42576b0654e8b07c4c0ec6529479f9ab9f5d
Victor <victor.j.zhao@gmail.com>
2022-04-10 16:45:48 -0400

    Spinner works

--- src/main/java/ca/mcgill/ecse/divesafe/JavaFx/controllers/StartTripController.java
+++ src/main/java/ca/mcgill/ecse/divesafe/JavaFx/controllers/StartTripController.java
@@ -1,19 +1,24 @@
 package ca.mcgill.ecse.divesafe.JavaFx.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import ca.mcgill.ecse.divesafe.application.DiveSafeApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.input.MouseEvent;
 
-public class StartTripController {
+public class StartTripController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -26,6 +31,18 @@ public class StartTripController {
 
     @FXML
     private Button resetButton;
+
+    // Configure spinner to choose desired week to start trips
+    @FXML
+    private Spinner<Integer> weekSpinner;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5, 1);
+        weekSpinner.setValueFactory(valueFactory);
+    }
+    
 
     @FXML
     void resetApp(MouseEvent event) {
